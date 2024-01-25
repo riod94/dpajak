f2462671262a79644b0fdb10a314cce959ad4557
Rio D <riodwi@nusa.net.id>
2024-01-25 22:57:08 +0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,17 +1,21 @@
-import { siteConfig } from "@/config/site";
 import { Link } from "@nextui-org/link";
 
 export default function Footer() {
+	const startYear = 2024;
+	const currentYear = new Date().getFullYear();
+
+	const year =
+		currentYear > startYear ? `${startYear}-${currentYear}` : startYear;
 	return (
 		<footer className="w-full flex items-center justify-center py-3">
 			<Link
-				isExternal
+				// isExternal
 				className="flex items-center gap-1 text-current"
-				href={siteConfig.links.sponsor}
+				href={"/"}
 				title="nextui.org homepage"
 			>
-				<span className="text-default-600">Powered by</span>
-				<p className="text-primary">Nusawork</p>
+				<span className="text-default-600">&copy; {year}</span>
+				<p className="text-primary">KPAJAK</p>
 			</Link>
 		</footer>
 	);
